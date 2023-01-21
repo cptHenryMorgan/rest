a3de5c4f943c1b2407586008120b47887f312d64
Artem Egorov <Artem-Egorov13@mail.ru>
2023-01-21 17:39:56 +0300

    Change appveor and test rub & usd

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,8 +1,9 @@
 package ru.netology.rest;
 
 import  org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV1 {
     @Test
@@ -15,7 +16,10 @@ class MobileBankApiTestV1 {
       .when()
           .get("/demo/accounts")
       // Проверки
-      .then()
-          .statusCode(200);
+              .then()
+              .statusCode(200)
+              .body("[0].currency" , equalTo("RUB"))
+              .body("[1].currency" , equalTo("USD"))
+              .body("[2].currency" , equalTo("RUB"));
     }
 }
